73a4cfd69f977ad8ef68be9cab23045f932ba791
jinjin2017c <746806139@qq.com>
2017-11-12 18:28:04 +0800

    datastructure/

--- datastructure/datastructure.c
+++ datastructure/datastructure.c
@@ -21,12 +21,12 @@ typedef struct
     Elemtype *elem ;       //
     int length;            //
     int size;              //
-}Sqlist;
+}SqList;
 
 /*
  * 
  */
-Status InitList(Sqlist *L)
+Status InitList(SqList *L)
 {
     L->elem =(Elemtype *) malloc(INIT_SIZE * sizeof(Elemtype));
     if(!L->elem) 
@@ -43,10 +43,139 @@ Status InitList(Sqlist *L)
  */
 Status ClearList(SqList *L)
 {
-    L->length = 0 ;
-    return OK ;
+    free(L->elem);
+    L->length = 0;
+    L->size = 0;
+    return OK;
 }
-
-
+/*
+ *
+ */
+Status ClearList(SqList *L)
+{
+    L->length = 0;
+    return OK;
+}
+/*
+ * 
+ */
+Status isEmpty(const SqList L)
+{
+    if(0 == L.length)
+    {
+        return TURE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+/*
+ *
+ */
+Status getLength(const SqList L)
+{
+    return L.length;
+}
+/*
+ * 
+ */
+Status GetElem(const SqList L, int i, Elemtype *e)
+{
+    if(i < 1||i > L.length)
+    {
+	return ERROE;
+    }
+    *e = L.elem[i-1];
+    return OK;
+}
+/*
+ *
+ */
+Status compare(Elemtype e1,Elemtype e2)
+{
+    if(e1 == e2)
+    {
+        return 0;
+    }
+    else if (e1<e2)
+    {
+	return -1;
+    }
+    else
+    {
+        return 1;
+    }
+}
+/*
+ *
+ */
+status FindElem(const SqList L,Elemtype e,Status(*compare)(Elemtype,Elemtype))
+// yu qian yi ge hanshu de cixu qianhou huiyouguanxi ma ?
+{
+    int i;
+    for (i = 0; i < L.length; i++)
+    {
+	if(!(*compare)(L.elem[i],e))
+        {
+            return i + 1;
+        }
+    }
+    if(i >= L.length)
+    {
+        return ERROR;
+    }
+}
+/*
+ *
+ */
+Status PreElem(const SqList L,Elemtype cur_e,Elemtype *pre_e)
+{
+    int i;
+    for (i = 0; i < L.length; i++)
+    {
+	if(cur_e == L.elem[i])
+        {
+	    if(i != 0)
+            {
+		*pre_e = L.elem[i-1];
+	    }
+            else
+	    {
+		return ERROR;
+	    }
+	}
+    }
+    if(i >= L.length)
+    {
+	return ERROR;
+    }
+} 
+/*
+ *
+ */
+Status NextElem(const SqList L,Elemtype cur_e, Elemtype *next_e)
+{
+    int i;
+    for(i = 0; i < L.length; i++)
+    {
+	if(cur_e == L.elem[i] )
+	{    
+            if(i < L.length -1)
+	    {
+		*next_e = L.elem[i+1];
+		return OK;
+	    }
+	    else
+     	    {
+       	        return ERROR;
+	    }
+	}
+    }
+    if(i >= L.length)
+    {
+	return ERROR;
+    }
+}
 
 
